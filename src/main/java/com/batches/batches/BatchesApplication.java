e0c9e2728879021b525ebb765747dd72d17fdbb7
PB <xachineon@gmail.com>
2025-03-24 18:36:18 +0530

    Configured Postgres Spring data JDBC.

--- src/main/java/com/batches/batches/BatchesApplication.java
+++ src/main/java/com/batches/batches/BatchesApplication.java
@@ -1,9 +1,11 @@
 package com.batches.batches;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class BatchesApplication {
 
 	public static void main(String[] args) {
