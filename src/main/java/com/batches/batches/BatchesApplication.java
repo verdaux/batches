15317623e95d041fe6ae9fa718b9d5b95064bb79
PB <xachineon@gmail.com>
2025-03-24 16:41:11 +0530

    Initial commit for a barebones architecture.

---
+++ src/main/java/com/batches/batches/BatchesApplication.java
@@ -0,0 +1,13 @@
+package com.batches.batches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/batches/batches/BatchesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batches.batches;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
